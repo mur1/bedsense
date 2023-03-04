6353e9d2cdb1134af9aa825d2237d71dd2092f9b
muri <106119896+mur1@users.noreply.github.com>
2023-03-04 15:13:25 -0300

    Create main.lua

---
+++ Luas/main.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 6872265039 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/mur1/bedsense/main/Luas/bedsense.lua'))()
+    return
+end
+game.Players.LocalPlayer:Kick("BedSense - No Game Detected!")
